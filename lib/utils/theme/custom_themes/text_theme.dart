7ef4a4fe9696609a2c49157c29243359f3daca32
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-02-23 02:20:56 +0530

    defined dark and light theme styles

---
+++ lib/utils/theme/custom_themes/text_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class GTextTheme {
+  GTextTheme._();
+
+  static TextTheme lightTextTheme = TextTheme();
+  
+  static TextTheme darkTextTheme = TextTheme();
+}
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
-
+import 'custom_themes/text_theme.dart';
 class GAppTheme {
   GAppTheme._();
 
-  static ThemeData lightTheme = ThemeData();
-  static ThemeData darkTheme = ThemeData.dark();
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: GTextTheme.lightTextTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: GTextTheme.darkTextTheme,
+  );
 }
