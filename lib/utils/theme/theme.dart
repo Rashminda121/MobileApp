88656675f5c509e856d078cf11e2ba0a2568629d
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-02-23 16:23:19 +0530

    themes

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'custom_themes/text_theme.dart';
+import 'custom_themes/elevated_button_theme.dart';
 class GAppTheme {
   GAppTheme._();
 
@@ -7,16 +8,18 @@ class GAppTheme {
     useMaterial3: true,
     fontFamily: 'Poppins',
     brightness: Brightness.light,
-    primaryColor: Colors.blue,
+    primaryColor: Colors.green,
     scaffoldBackgroundColor: Colors.white,
     textTheme: GTextTheme.lightTextTheme,
+    elevatedButtonTheme:GElevatedButtonTheme.lightElevatedButtonTheme,
   );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
     brightness: Brightness.dark,
-    primaryColor: Colors.blue,
+    primaryColor: Colors.green,
     scaffoldBackgroundColor: Colors.black,
     textTheme: GTextTheme.darkTextTheme,
+    elevatedButtonTheme:GElevatedButtonTheme.darkElevatedButtonTheme,
   );
 }
