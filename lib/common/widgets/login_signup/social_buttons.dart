317f5a46049f0bc545e56c4038fd9a5386212e30
Rashminda <rashminda121@gmail.com>
2024-03-06 23:14:16 +0530

    social button dart file added

---
+++ lib/common/widgets/login_signup/social_buttons.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/image_strings.dart';
+import '../../../utils/constants/sizes.dart';
+
+class TSocialButtons extends StatelessWidget {
+  const TSocialButtons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              border: Border.all(color: TColors.grey),
+              borderRadius: BorderRadius.circular(100)),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Image(
+              width: TSizes.iconMd,
+              height: TSizes.iconMd,
+              image: AssetImage(TImages.google),
+            ),
+          ),
+        ),
+        const SizedBox(width: TSizes.spaceBtwItems),
+        Container(
+          decoration: BoxDecoration(
+              border: Border.all(color: TColors.grey),
+              borderRadius: BorderRadius.circular(100)),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Image(
+              width: TSizes.iconMd,
+              height: TSizes.iconMd,
+              image: AssetImage(TImages.facebook),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
