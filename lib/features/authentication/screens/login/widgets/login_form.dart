2aa0ae6eab62c10d0f14cf2de3c94ec9f72a61e3
Rashminda <rashminda121@gmail.com>
2024-03-06 23:14:47 +0530

    added login form

---
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../../utils/constants/sizes.dart';
+import '../../../../../utils/constants/text_strings.dart';
+
+class TLoginForm extends StatelessWidget {
+  const TLoginForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: TSizes.spaceBtwSections),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Iconsax.direct_right),
+                labelText: TTexts.email,
+              ),
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwInputFields,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Iconsax.password_check),
+                labelText: TTexts.password,
+                suffixIcon: Icon(Iconsax.eye_slash),
+              ),
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwInputFields / 2,
+            ),
+
+            ///remember me and forget password
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ///remember me
+                Row(
+                  children: [
+                    Checkbox(value: true, onChanged: (value) {}),
+                    const Text(TTexts.rememberMe),
+                  ],
+                ),
+
+                ///forget password
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(TTexts.forgetPassword),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwSections,
+            ),
+
+            /// Sign In Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(TTexts.signIn),
+              ),
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwItems,
+            ),
+
+            /// Create Account Button
+            SizedBox(
+              width: double.infinity,
+              child: OutlinedButton(
+                onPressed: () {},
+                child: const Text(TTexts.createAccount),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
