e1520ac26643ce34bd9e9dcf0222da9215ea445f
Rashminda <rashminda121@gmail.com>
2024-03-06 23:14:02 +0530

    form divider dart file added

---
+++ lib/common/widgets/login_signup/login_form_divider.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../utils/constants/colors.dart';
+
+class TFormDivider extends StatelessWidget {
+  const TFormDivider({
+    super.key,
+    required this.dark,
+    required this.dividerText,
+  });
+
+  final String dividerText;
+  final bool dark;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Flexible(
+            child: Divider(
+              color: dark ? TColors.darkGrey : TColors.grey,
+              thickness: 0.5,
+              indent: 60,
+              endIndent: 5,
+            )),
+        Text(
+          dividerText,
+          style: Theme.of(context).textTheme.labelMedium,
+        ),
+        Flexible(
+            child: Divider(
+              color: dark ? TColors.darkGrey : TColors.grey,
+              thickness: 0.5,
+              indent: 5,
+              endIndent: 60,
+            ))
+      ],
+    );
+  }
+}
