b823e2538bb0c5c7749bdf3a20800b23751ef9ca
avish madushanka <avishmadushanka21@gmail.com>
2024-03-22 23:16:14 +0530

    create shop page

---
+++ lib/features/shop/screens/product_details/product_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductDetail extends StatelessWidget {
+  const ProductDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: SingleChildScrollView(child: Column()));
+  }
+}
