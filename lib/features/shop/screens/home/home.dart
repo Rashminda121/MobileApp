305a16d487e2c09fca9a644201b203d5cd005b25
Rashminda <rashminda121@gmail.com>
2024-03-24 21:10:05 +0530

    Update home.dart

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,26 @@
+
 import 'package:flutter/material.dart';
+import '../../../../common/widgets/custom_shapes/container/primary_header_container.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryheaderContainer(
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
+
