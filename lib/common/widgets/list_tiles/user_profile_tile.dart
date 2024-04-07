97b93e5fb773c376eafe12f838379b0cb4bbafe2
Rashminda <rashminda121@gmail.com>
2024-04-08 00:12:01 +0530

    Update user_profile_tile.dart

--- lib/common/widgets/list_tiles/user_profile_tile.dart
+++ lib/common/widgets/list_tiles/user_profile_tile.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
-class TUserProfileTile extends StatelessWidget {
-  const TUserProfileTile({super.key});
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/image_strings.dart';
+import '../images/t_circular_image.dart';
+
+class TUserProfileTitle extends StatelessWidget {
+  const TUserProfileTitle({
+    super.key,
+    required this.onPressed,
+  });
+
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return ListTitle(
+    return ListTile(
       leading: const TCircularImage(
-          image: TImages.user, width: 50, height: 50, padding: 0),
-      title: Text('Coding with T',
-          style: Theme.of(context)
-              .textTheme
-              .headlineSmall!
-              .apply(color: TColors.white)),
-      subtitile: Text('support@example.com',
-          style: Theme.of(context)
-              .textTheme
-              .bodyMedium!
-              .apply(color: TColors.white)),
+        image: TImages.user,
+        width: 50,
+        height: 50,
+        padding: 0,
+      ),
+      title: Text(
+        'User Name',
+        style: Theme.of(context)
+            .textTheme
+            .headlineSmall!
+            .apply(color: TColors.white),
+      ),
+      subtitle: Text(
+        'testemail@email.com',
+        style:
+            Theme.of(context).textTheme.bodyMedium!.apply(color: TColors.white),
+      ),
       trailing: IconButton(
-          onPressed: (), icon: const Icon(Iconsax.edit, color: TColors.white)),
+          onPressed: onPressed,
+          icon: const Icon(Iconsax.edit, color: TColors.white)),
     );
   }
 }
