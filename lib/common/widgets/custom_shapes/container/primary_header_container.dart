5beeb56127c3eafc5fbfd55aaecae1d98d11fda3
Rashminda <rashminda121@gmail.com>
2024-04-08 00:11:39 +0530

    Update primary_header_container.dart

--- lib/common/widgets/custom_shapes/container/primary_header_container.dart
+++ lib/common/widgets/custom_shapes/container/primary_header_container.dart
@@ -16,28 +16,14 @@ class TPrimaryheaderContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return TCurvedEdgesWidget(
       child: Container(
-        color: TColors.primaryColor,
-        padding: const EdgeInsets.all(0),
-          child: SizedBox(
-            height: 400,
-            child: Stack(
-              children: [
-                Positioned(
-                    top: -150,
-                    right: -250,
-                    child: TCirculatContainer(
-                      backgroundColor: TColors.textWhite.withOpacity(0.1),
-                    )),
-                Positioned(
-                  top: 100,
-                  right: -300,
-                  child: TCirculatContainer(
-                    backgroundColor: TColors.textWhite.withOpacity(0.1),
-                  ),
-                ),
-              ],
-            ),
-          ),
+        color: TColors.primary,
+        child: Stack(
+          children: [
+            Positioned(top: -150, right: -250, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
+            Positioned(top: 100, right: -300, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.081))),
+            child,
+          ],
+        ),
       ),
     );
   }
