1b73483117ac176a626fffb4b7560768768271d7
Rashminda <rashminda121@gmail.com>
2024-04-08 00:13:45 +0530

    Create promo_slider.dart

---
+++ lib/features/shop/screens/home/widgets/promo_slider.dart
@@ -0,0 +1,49 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../../common/widgets/custom_shapes/container/circular_container.dart';
+import '../../../../../common/widgets/images/t_rounded_image.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/sizes.dart';
+import '../../../controllers/home_controller.dart';
+
+class TPromoSlider extends StatelessWidget {
+  const TPromoSlider({
+    super.key, required this.banners,
+  });
+
+  final List<String> banners;
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(HomeController());
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+              viewportFraction: 1,
+              onPageChanged: (index, _) =>
+                  controller.updatePageIndicator(index)),
+          items: banners.map((url) => TRoundedImage(imageUrl: url)).toList()
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        Center(
+          child: Obx(
+            () => Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                for (int i = 0; i < banners.length; i++)
+                  TCircularContainer(
+                      width: 20,
+                      height: 4,
+                      margin: const EdgeInsets.only(right: 10),
+                      backgroundColor: controller.carousalCurrentIndex.value == i? TColors.primary:TColors.grey),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
