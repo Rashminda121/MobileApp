14185e982e0124e9e3ad7d83eec337f72e88829c
Rashminda <rashminda121@gmail.com>
2024-04-08 00:13:36 +0530

    Create billing_amount_section.dart

---
+++ lib/features/shop/screens/checkout/widgets/billing_amount_section.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../utils/constants/sizes.dart';
+
+class TBillingAmountSection extends StatelessWidget {
+  const TBillingAmountSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        /// SubTotal
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Subtotal', style: Theme.of(context).textTheme.bodyMedium),
+            Text('\$256.0', style: Theme.of(context).textTheme.bodyMedium),
+          ],
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems/2),
+
+
+        /// shipping fee
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Shipping fee', style: Theme.of(context).textTheme.bodyMedium),
+            Text('\$6.0', style: Theme.of(context).textTheme.labelLarge),
+          ],
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems/2),
+
+        /// Tax fee
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Tax feel', style: Theme.of(context).textTheme.bodyMedium),
+            Text('\$6.0', style: Theme.of(context).textTheme.labelLarge),
+          ],
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems/2),
+
+        /// Order Total
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Order Total', style: Theme.of(context).textTheme.bodyMedium),
+            Text('\$6.0', style: Theme.of(context).textTheme.titleMedium),
+          ],
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems/2),
+
+
+      ],
+    );
+  }
+}
