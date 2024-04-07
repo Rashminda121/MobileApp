7bbdeaa1ecd7cd7be0165123cad3c86ddbda4b3b
Rashminda <rashminda121@gmail.com>
2024-04-08 00:13:26 +0530

    Create brand_product.dart

---
+++ lib/features/shop/screens/brand/brand_product.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../../common/widgets/appbar/appbar.dart';
+import '../../../../common/widgets/brands/brand_card.dart';
+import '../../../../common/widgets/products/sortable/sortable_products.dart';
+import '../../../../utils/constants/sizes.dart';
+
+class BrandProducts extends StatelessWidget {
+  const BrandProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: TAppBar(title: Text('Nike')),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Brand Detail
+              TBrandCard(showBorder: true),
+              SizedBox(height: TSizes.spaceBtwSections),
+              TSortableProducts(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
