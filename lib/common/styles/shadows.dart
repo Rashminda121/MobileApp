e413a05359db0428bc48e9b6f653203b4f13ead2
Rashminda <rashminda121@gmail.com>
2024-04-08 00:11:26 +0530

    Create shadows.dart

---
+++ lib/common/styles/shadows.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/constants/colors.dart';
+
+class TShadowStyle {
+  static final verticalProductShadow = BoxShadow(
+    color: TColors.darkGrey.withOpacity(0.1),
+    blurRadius: 50,
+    spreadRadius: 7,
+    offset: const Offset(0, 2),
+  ); // BoxShadow
+
+  static final horizontalProductShadow = BoxShadow(
+    color: TColors.darkGrey.withOpacity(0.1),
+    blurRadius: 50,
+    spreadRadius: 7,
+    offset: const Offset(0, 2),
+  ); // BoxShadow
+}
