3fbb1ccc7f49238c7412e9d313d86a5775175a1c
Rashminda <rashminda121@gmail.com>
2024-04-08 00:13:08 +0530

    Update single_address.dart

--- lib/features/personalization/screens/address/widgets/single_address.dart
+++ lib/features/personalization/screens/address/widgets/single_address.dart
@@ -1,42 +1,42 @@
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:iconsax/iconsax.dart';
 
+import '../../../../../common/styles/rounded_container.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/sizes.dart';
+import '../../../../../utils/helpers/helper_functions.dart';
+
 class TSingleAddress extends StatelessWidget {
-  const TSingleAddress({super.key});
+  const TSingleAddress({
+    super.key, required this.selectedAddress,
+  });
 
   final bool selectedAddress;
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
     return TRoundedContainer(
-        padding: const EdgeInsets.all(TSizes.md),
+      padding: const EdgeInsets.all(TSizes.md),
+      showBorder: true,
         width: double.infinity,
-        showBorder: true,
-        backgroundColor: selectedAddress
-            ? TColors.primary.withOpacity(0.5)
-            : dark
-                ? TColors.dark
-                : TColors.transparent,
-        borderColor: selectedAddress
-            ? Colors.transparent
-            : dark
-                ? TColors.darkGrey
-                : TColors.grey,
-        margin: const EdgeInsets.only(bottom: TSizes.spaceBtwItems),
-        child: Stack(children: [
+        backgroundColor: selectedAddress ?TColors.primary.withOpacity(0.5): Colors.transparent,
+        borderColor: selectedAddress? Colors.transparent: dark?TColors.darkerGrey : TColors.grey,
+      margin: const EdgeInsets.only(bottom: TSizes.spaceBtwItems),
+      child: Stack(
+        children: [
           Positioned(
             right: 5,
             top: 0,
+
             child: Icon(
               selectedAddress ? Iconsax.tick_circle5 : null,
               color: selectedAddress
-                  ? dark
-                      ? TColors.light
-                      : TColors.dark
+                  ? (dark ? TColors.light : TColors.dark.withOpacity(0.6))
                   : null,
             ),
           ),
+
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -47,18 +47,18 @@ class TSingleAddress extends StatelessWidget {
                 style: Theme.of(context).textTheme.titleLarge,
               ),
               const SizedBox(height: TSizes.sm / 2),
-              const Text(
-                '0785695874',
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
+              const Text('(+123) 456 7890', maxLines: 1, overflow: TextOverflow.ellipsis),
               const SizedBox(height: TSizes.sm / 2),
-              const Text(
-                '54544 Timmy Coves, South Liana, Mine, 56454, USA',
-                softWrap: true,
-              )
+              const Text('82356 Timmy Coves, South Liana, Maine, 87665, USA', softWrap: true),
+
+
             ],
-          )
-        ]));
+          ),
+
+
+
+        ],
+      ),
+    );
   }
 }
