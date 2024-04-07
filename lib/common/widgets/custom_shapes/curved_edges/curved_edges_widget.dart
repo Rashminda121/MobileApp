b6b65e4b651637bac37b20b49fd5018da25b1367
Rashminda <rashminda121@gmail.com>
2024-04-08 00:11:45 +0530

    Update curved_edges_widget.dart

--- lib/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart
+++ lib/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_app/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+
+import 'curved_edges.dart';
 
 class TCurvedEdgesWidget extends StatelessWidget {
   const TCurvedEdgesWidget({
@@ -17,3 +18,4 @@ class TCurvedEdgesWidget extends StatelessWidget {
     );
   }
 }
+
