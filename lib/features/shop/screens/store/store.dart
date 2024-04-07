5724919f349eb31c0055d24267499a9596f26c81
Rashminda <rashminda121@gmail.com>
2024-04-08 00:14:29 +0530

    Create store.dart

---
+++ lib/features/shop/screens/store/store.dart
@@ -0,0 +1,106 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:mobile_app/features/shop/screens/store/widgets/category_tab.dart';
+import '../../../../common/widgets/appbar/appbar.dart';
+import '../../../../common/widgets/appbar/tabbar.dart';
+import '../../../../common/widgets/brands/brand_card.dart';
+import '../../../../common/widgets/custom_shapes/container/search_container.dart';
+import '../../../../common/widgets/layouts/grid_layout.dart';
+import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../common/widgets/texts/section_heading.dart';
+import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/helpers/helper_functions.dart';
+import '../brand/all_brands.dart';
+
+class StoreScreen extends StatelessWidget {
+  const StoreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: TAppBar(
+          title:
+              Text('Store', style: Theme.of(context).textTheme.headlineMedium),
+          actions: [
+            TCartCounterIcon(onPressed: () {}),
+          ],
+        ),
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  pinned: true,
+                  floating: true,
+                  backgroundColor: THelperFunctions.isDarkMode(context)
+                      ? TColors.black
+                      : TColors.white,
+                  expandedHeight: 440,
+                  flexibleSpace: Padding(
+                      padding: const EdgeInsets.all(TSizes.defaultSpace),
+                      child: ListView(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: [
+                          ///Search Bar
+                          const SizedBox(height: TSizes.spaceBtwItems),
+                          const TSearchContainer(
+                              text: 'Search in Store',
+                              showBorder: true,
+                              showBackground: false,
+                              padding: EdgeInsets.zero),
+                          const SizedBox(height: TSizes.spaceBtwSections),
+
+                          ///featured brands
+                          TSectionHeading(
+                              title: 'Featured Brands', onPressed: ()=>Get.to(()=>const AllBrandsScreen())),
+                          const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+
+                          TGridLayout(
+                              itemCount: 4,
+                              mainAxisExtent: 80,
+                              itemBuilder: (_, index) {
+                                return const TBrandCard(showBorder: false);
+                              }),
+                        ],
+                      )),
+
+                  ///tabs----
+                  bottom: const TTabBar(
+                    tabs: [
+                      Tab(child: Text('Sports')),
+                      Tab(child: Text('Furniture')),
+                      Tab(child: Text('Electronics')),
+                      Tab(child: Text('Clothes')),
+                      Tab(child: Text('Cosmetics')),
+                    ],
+                  )),
+            ];
+          },
+          body: const TabBarView(
+            children: [
+            TCategoryTab(),
+              TCategoryTab(),
+              TCategoryTab(),
+              TCategoryTab(),
+              TCategoryTab()
+
+
+
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
