c7f8a67853408140a9158a8e9358f838763e4b1b
Rashminda <rashminda121@gmail.com>
2024-04-08 00:12:46 +0530

    Create forget_password.dart

---
+++ lib/features/authentication/screens/password_configuration/forget_password.dart
@@ -0,0 +1,62 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:mobile_app/features/authentication/screens/password_configuration/reset_password.dart';
+
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/constants/text_strings.dart';
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Headings
+            Text(
+              TTexts.forgetPasswordTitle,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwItems,
+            ),
+            Text(
+              TTexts.forgetPasswordSubTitle,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwSections * 2,
+            ),
+
+            // Text field
+
+            TextFormField(
+              decoration: const InputDecoration(
+                  labelText: TTexts.email,
+                  prefixIcon: Icon(Iconsax.direct_right)),
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwSections,
+            ),
+
+            // Submit Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () => Get.off(() => const ResetPassword()),
+                  child: const Text(TTexts.submit)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
