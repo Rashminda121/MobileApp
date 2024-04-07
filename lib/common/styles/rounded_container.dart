b573af5fef3ac7beb2b8cb37dd8b498139eeec9e
Rashminda <rashminda121@gmail.com>
2024-04-08 00:11:25 +0530

    Create rounded_container.dart

---
+++ lib/common/styles/rounded_container.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/constants/colors.dart';
+import '../../utils/constants/sizes.dart';
+
+class TRoundedContainer extends StatelessWidget {
+  const TRoundedContainer({
+    super.key,
+    this.child,
+    this.width,
+    this.height,
+    this.margin,
+    this.padding,
+    this.showBorder = false,
+    this.radius = TSizes.cardRadiusLg,
+    this.backgroundColor = TColors.white,
+    this.borderColor = TColors.borderPrimary,
+  });
+
+  final double? width;
+  final double? height;
+  final double radius;
+  final Widget? child;
+  final bool showBorder;
+  final Color borderColor;
+  final Color backgroundColor;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: padding,
+      margin: margin,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(radius),
+        border: showBorder ? Border.all(color: borderColor) : null,
+      ),
+      child: child,
+    );
+  }
+}
