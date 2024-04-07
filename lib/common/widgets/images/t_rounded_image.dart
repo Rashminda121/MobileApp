a6906dbcfedcf5427a7e5b9d5a2e8456af5d6fa1
Rashminda <rashminda121@gmail.com>
2024-04-08 00:11:53 +0530

    Create t_rounded_image.dart

---
+++ lib/common/widgets/images/t_rounded_image.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/sizes.dart';
+
+class TRoundedImage extends StatelessWidget {
+  const TRoundedImage({
+    super.key,
+    this.border,
+    this.padding,
+    this.onPressed,
+    this.width,
+    this.height,
+    required this.imageUrl,
+    this.applyImageRadius = true,
+    this.backgroundColor,
+    this.fit = BoxFit.contain,
+    this.isNetworkImage = false,
+    this.borderRadius=TSizes.md,
+  });
+
+  final double? width;
+  final double? height;
+  final String imageUrl;
+  final bool applyImageRadius;
+  final BoxBorder? border;
+  final Color? backgroundColor;
+  final BoxFit? fit;
+  final EdgeInsetsGeometry? padding;
+  final bool isNetworkImage;
+  final VoidCallback? onPressed;
+  final double borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: width,
+        height: height,
+        padding: padding,
+        decoration: BoxDecoration(
+          border: border,
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+        child: ClipRRect(
+          borderRadius: applyImageRadius
+              ? BorderRadius.circular(borderRadius)
+              : BorderRadius.zero,
+          child: Image(
+            fit: fit,
+            image: isNetworkImage
+                ? NetworkImage(imageUrl)
+                : AssetImage(imageUrl) as ImageProvider,
+          ),
+        ),
+      ),
+    );
+  }
+}
