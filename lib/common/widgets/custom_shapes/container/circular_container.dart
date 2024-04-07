8ec588bfe9376ca0292cd4fccbb2dc94baebd7ac
Rashminda <rashminda121@gmail.com>
2024-04-08 00:11:37 +0530

    Update circular_container.dart

--- lib/common/widgets/custom_shapes/container/circular_container.dart
+++ lib/common/widgets/custom_shapes/container/circular_container.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 
 import '../../../../utils/constants/colors.dart';
 
-class TCirculatContainer extends StatelessWidget {
-  const TCirculatContainer({
+class TCircularContainer extends StatelessWidget {
+  const TCircularContainer({
     super.key,
     this.child,
     this.width= 400,
     this.height= 400,
     this.radius=400,
     this.padding=0,
+    this.margin,
     this.backgroundColor= TColors.white,
   });
 
@@ -17,6 +18,7 @@ class TCirculatContainer extends StatelessWidget {
   final double? height;
   final double radius;
   final double padding;
+  final EdgeInsets? margin;
   final Widget? child;
   final Color backgroundColor;
 
@@ -25,6 +27,7 @@ class TCirculatContainer extends StatelessWidget {
     return Container(
       width: width,
       height: height,
+      margin: margin,
       padding: EdgeInsets.all(padding),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(radius),
