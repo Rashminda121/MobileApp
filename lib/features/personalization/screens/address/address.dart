a76759b51f469937cc1ff51b5747d61f85315687
Rashminda <rashminda121@gmail.com>
2024-04-08 00:13:06 +0530

    Update address.dart

--- lib/features/personalization/screens/address/address.dart
+++ lib/features/personalization/screens/address/address.dart
@@ -1,37 +1,41 @@
-import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/get_navigation.dart';
-import 'package:mobile_app/features/personalization/screens/address/widgets/single_address.dart';
-import 'package:iconsax/iconsax.dart';
 
-class UserAddressScreen extends StatelessWidget {
-  const UserAddressScreen({super.key});
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:mobile_app/features/personalization/screens/address/widgets/single_address.dart';
+
+import '../../../../common/widgets/appbar/appbar.dart';
+import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/sizes.dart';
+import 'add_new_addres.dart';
+
+class UserAdressScreen extends StatelessWidget {
+  const UserAdressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         backgroundColor: TColors.primary,
-        onPressed: () => Get.to(() => const addNewAddressScreen()),
-        child: const Icon(
-          Iconsax.add,
-          color: TColors.white,
-        ),
+        onPressed: () => Get.to(() => const AddNewAddressScreen()),
+        child: const Icon(Iconsax.add, color: TColors.white),
       ),
       appBar: TAppBar(
         showBackArrow: true,
-        title:
-            Text('Addresses', style: Theme.of(context).textTheme.headlineSmall),
+        title: Text('Addresses', style: Theme.of(context).textTheme.headlineSmall),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-            padding: EdgeInsets.all(TSizes.defaultSpace),
-            child: const Column(
-              children: [
-                TSingleAddress(selectedAddress: true),
+
+      body: const SingleChildScrollView(
+        child: Padding (
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column (
+            children: [
                 TSingleAddress(selectedAddress: false),
-              ],
-            )),
+              TSingleAddress(selectedAddress: true),
+
+            ],
+          ),
+        ),
       ),
     );
   }
