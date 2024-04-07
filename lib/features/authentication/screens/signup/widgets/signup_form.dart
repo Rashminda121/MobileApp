7912ecd6fa4a1cafe02bd1114b87e354ca17f064
Rashminda <rashminda121@gmail.com>
2024-04-08 00:12:55 +0530

    Update signup_form.dart

--- lib/features/authentication/screens/signup/widgets/signup_form.dart
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -1,94 +1,105 @@
+
 import 'package:flutter/material.dart';
-import 'package:mobile_app/utils/constants/colors.dart';
-import 'package:mobile_app/utils/constants/sizes.dart';
-import 'package:mobile_app/utils/constants/text_strings.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:mobile_app/utils/helpers/helper_functions.dart';
-import 'package:mobile_app/features/authentication/screens/signup/widgets/terms_conditions_checkbox.dart';
+import 'package:mobile_app/features/authentication/screens/signup/widgets/terms_and_conditions_checkbox.dart';
+
+import '../../../../../utils/constants/sizes.dart';
+import '../../../../../utils/constants/text_strings.dart';
+import '../../../../../utils/helpers/helper_functions.dart';
+import '../verify_email.dart';
 
 class TSignupForm extends StatelessWidget {
   const TSignupForm({
     super.key,
-    
   });
 
-  
   @override
   Widget build(BuildContext context) {
-    
-    return Form(child: Column(
-      children:[
-        Row(
-          children: [
-            Expanded(
-              child: TextFormField(
-                expands: false,
-                decoration: const InputDecoration(
-                  labelText: TTexts.firstName,
-                  prefixIcon: Icon(Iconsax.user)),
+    final dark = THelperFunctions.isDarkMode(context);
+    return Form(
+      child: Column(
+        children: [
+          ///first name & last name
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  expands: false,
+                  decoration: const InputDecoration(
+                    labelText: TTexts.firstName,
+                    prefixIcon: Icon(Iconsax.user),
+                  ),
+                ),
               ),
-            ), 
-            const SizedBox(width: TSizes.spaceBtwInputFields),
-            Expanded(
-              child: TextFormField(
-                expands: false,
-                decoration: const InputDecoration(
-                  labelText: TTexts.lastName,
-                  prefixIcon: Icon(Iconsax.user)),
+              const SizedBox(width: TSizes.spaceBtwInputFields),
+              Expanded(
+                child: TextFormField(
+                  expands: false,
+                  decoration: const InputDecoration(
+                    labelText: TTexts.lastName,
+                    prefixIcon: Icon(Iconsax.user),
+                  ),
+                ),
               ),
-            ),   
-          ],
-        ),
-        const SizedBox(height: TSizes.spaceBtwInputFields),
-    
-        ///username
-        TextFormField(
-          expands: false,
-          decoration: const InputDecoration(
-            labelText: TTexts.username,
-            prefixIcon: Icon(Iconsax.user_edit)),
-        ),
-      const SizedBox(height: TSizes.spaceBtwInputFields),
-      ///email
-      TextFormField(
-          decoration: const InputDecoration(
-            labelText: TTexts.email,
-            prefixIcon: Icon(Iconsax.direct)),
-        ),
-    
-      const SizedBox(height: TSizes.spaceBtwInputFields),
-    
-      ///phone number
-      TextFormField(
-          decoration: const InputDecoration(
-            labelText: TTexts.phoneNo,
-            prefixIcon: Icon(Iconsax.call)),
-        ),
-    
-    
-      const SizedBox(height: TSizes.spaceBtwInputFields),
-    
-      ///password
-      TextFormField(
-          obscureText: true,
-          decoration: const InputDecoration(
-            labelText: TTexts.password,
-            prefixIcon: Icon(Iconsax.password_check),
-            suffixIcon: Icon(Iconsax.eye_slash),
-        ),
+            ],
+          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          /// Username
+          TextFormField(
+            expands: false,
+            decoration: const InputDecoration(
+              labelText: TTexts.username,
+              prefixIcon: Icon(Iconsax.user_edit),
+            ),
+          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          /// Email
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: TTexts.email,
+              prefixIcon: Icon(Iconsax.direct),
+            ),
+          ),
+
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          /// Phone Number
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: TTexts.phoneNo,
+              prefixIcon: Icon(Iconsax.call),
+            ),
+          ),
+
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          ///password
+          TextFormField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              labelText: TTexts.password,
+              prefixIcon: Icon(Iconsax.password_check),
+              suffixIcon: Icon(Iconsax.eye_slash),
+            ),
+          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          ///term condition
+          const TTermsAndConditionCheckbox(),
+          const SizedBox(height: TSizes.spaceBtwSections),
+
+          ///Signup button
+
+          SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () => Get.to(() => const VerifyEmailScreen()),
+                  child: const Text(TTexts.createAccount))),
+        ],
       ),
-    
-      const SizedBox(height: TSizes.spaceBtwSections),
-    
-    
-      ///Terms & Conditions Checkbox
-      const TTermsAndConditionCheckbox(),
-      const SizedBox (height: TSizes.spaceBtwSections),
-      ///Sign Up Button
-      SizedBox(width:double.infinity, child: ElevatedButton(onPressed: (){}, child: const Text(TTexts.createAccount)))
-      ],
-    
-    ),);
+    );
   }
 }
-
