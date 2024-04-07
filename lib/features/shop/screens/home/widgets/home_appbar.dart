925340160bc4b619a18cd0696828c591a3970f12
Rashminda <rashminda121@gmail.com>
2024-04-08 00:13:41 +0530

    Create home_appbar.dart

---
+++ lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../common/widgets/appbar/appbar.dart';
+import '../../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/text_strings.dart';
+
+class THomeAppBar extends StatelessWidget {
+  const THomeAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TAppBar(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(TTexts.homeAppbarTitle, style: Theme.of(context).textTheme.labelMedium!.apply(color: TColors.grey)),
+          Text(TTexts.homeAppbarSubTitle, style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white)),
+        ],
+      ),
+      actions: [
+        TCartCounterIcon(onPressed: () {  }, iconColor: TColors.white,)
+      ],
+
+
+    );
+  }
+}
