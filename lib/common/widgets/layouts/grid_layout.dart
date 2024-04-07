c71e258841df89b9a66b922b5c2c7c6a79f12d16
Rashminda <rashminda121@gmail.com>
2024-04-08 00:11:55 +0530

    Create grid_layout.dart

---
+++ lib/common/widgets/layouts/grid_layout.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/sizes.dart';
+
+class TGridLayout extends StatelessWidget {
+  const TGridLayout({
+    super.key,
+    required this.itemCount,
+    required this.itemBuilder,
+    this.mainAxisExtent = 288,
+  });
+
+  final int itemCount;
+  final double? mainAxisExtent;
+  final Widget? Function(BuildContext, int) itemBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: itemCount,
+      shrinkWrap: true,
+      padding: EdgeInsets.zero,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisExtent: mainAxisExtent,
+        mainAxisSpacing: TSizes.gridViewSpacing,
+        crossAxisSpacing: TSizes.gridViewSpacing,
+      ),
+      itemBuilder: itemBuilder,
+    );
+  }
+}
