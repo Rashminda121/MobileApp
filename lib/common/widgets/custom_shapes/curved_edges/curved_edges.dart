8330d96fb50eb6dcfda56c4b3db242e3c2a74200
Rashminda <rashminda121@gmail.com>
2024-04-08 00:11:43 +0530

    Update curved_edges.dart

--- lib/common/widgets/custom_shapes/curved_edges/curved_edges.dart
+++ lib/common/widgets/custom_shapes/curved_edges/curved_edges.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
 
-class TCustomCurvedEdges extends CustomClipper<Path> {
+class TCustomCurvedEdges extends CustomClipper<Path>{
   @override
   Path getClip(Size size) {
-    var path = Path();
+    var path= Path();
     path.lineTo(0, size.height);
 
-    final firstCurve = Offset(0, size.height - 20);
-    final lastCurve = Offset(30, size.height - 20);
-    path.quadraticBezierTo(
-        firstCurve.dx, firstCurve.dy, lastCurve.dx, lastCurve.dy);
+    final firstCurve= Offset(0, size.height-20);
+    final lastCurve= Offset(30, size.height-20);
+    path.quadraticBezierTo(firstCurve.dx,firstCurve.dy, lastCurve.dx, lastCurve.dy);
 
     final secondFirstCurve = Offset(0, size.height - 20);
-    final secondLastCurve = Offset(size.width-30, size.height - 20);
-    path.quadraticBezierTo(
-        secondFirstCurve.dx, secondFirstCurve.dy, secondLastCurve.dx, secondLastCurve.dy);
-
+    final secondLastCurve = Offset(size.width - 30, size.height - 20);
+    path.quadraticBezierTo(secondFirstCurve.dx, secondFirstCurve.dy, secondLastCurve.dx, secondLastCurve.dy);
 
     final thirdFirstCurve = Offset(size.width, size.height - 20);
     final thirdLastCurve = Offset(size.width, size.height);
-    path.quadraticBezierTo(
-        thirdFirstCurve.dx, thirdFirstCurve.dy, thirdLastCurve.dx, thirdLastCurve.dy);
+    path.quadraticBezierTo(thirdFirstCurve.dx, thirdFirstCurve.dy, thirdLastCurve.dx, thirdLastCurve.dy);
+
     path.lineTo(size.width, 0);
     path.close();
     return path;
   }
 
   @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+  bool shouldReclip(covariant CustomClipper<dynamic> oldClipper) {
     return true;
   }
+
 }
