3a9625dd99e3e860d2164d9adf1fd98907fbb724
Rashminda <rashminda121@gmail.com>
2024-04-08 00:12:50 +0530

    Update signup.dart

--- lib/features/authentication/screens/signup/signup.dart
+++ lib/features/authentication/screens/signup/signup.dart
@@ -1,23 +1,22 @@
-import 'package:mobile_app/common/widgets/login_signup/social_buttons.dart';
-import 'package:mobile_app/features/authentication/screens/signup/widgets/signup_form.dart';
-import '../login/widgets/form_divider.dart';
-// import "../../../../common/widgets/login_signup/login_form_divider.dart";
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mobile_app/features/authentication/screens/signup/widgets/signup_form.dart';
 
-import 'package:mobile_app/utils/constants/text_strings.dart';
+
+import '../../../../common/widgets/login_signup/login_form_divider.dart';
+import '../../../../common/widgets/login_signup/social_buttons.dart';
 import '../../../../utils/constants/sizes.dart';
-import 'package:iconsax/iconsax.dart';
-
-import '../../../../utils/constants/colors.dart';
-
+import '../../../../utils/constants/text_strings.dart';
+import '../../../../utils/helpers/helper_functions.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
+    final dark = THelperFunctions.isDarkMode(context);
+
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
@@ -29,18 +28,27 @@ class SignupScreen extends StatelessWidget {
               ///title
               Text(
                 TTexts.signupTitle,
-                style:Theme.of(context).textTheme.headlineMedium),
-                const SizedBox(height: TSizes.spaceBtwSections),
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
 
-                ///form
-                const TSignupForm(),
-              const SizedBox (height: TSizes.spaceBtwSections),
-                ///Divider
-               TFormDivider(dividerText: TTexts.orSignInWith.capitalize!),
-              const SizedBox (height: TSizes.spaceBtwSections),
-              ///Social Buttons
+              ///form
+              const TSignupForm(),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              ///Divider
+              TFormDivider(dark: dark, dividerText: TTexts.orSignUpWith.capitalize!),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              /// Social Buttons
+
               const TSocialButtons(),
-              
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+
+
             ],
           ),
         ),
@@ -48,4 +56,5 @@ class SignupScreen extends StatelessWidget {
     );
   }
 }
+
 
