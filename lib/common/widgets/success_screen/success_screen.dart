2a278364964e07dd34208b83b943414613e61906
Rashminda <rashminda121@gmail.com>
2024-04-08 00:12:23 +0530

    Create success_screen.dart

---
+++ lib/common/widgets/success_screen/success_screen.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/material.dart';
+import '../../../utils/constants/sizes.dart';
+import '../../../utils/constants/text_strings.dart';
+import '../../../utils/helpers/helper_functions.dart';
+import '../../styles/spacing_styles.dart';
+
+class SuccessScreen extends StatelessWidget {
+  const SuccessScreen({super.key, required this.image, required this.title, required this.subTitle, required this.onPressed});
+
+  final String image,title,subTitle;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: TSpacingStyle.paddingWithAppBarHeight * 2,
+          child: Column(
+            children: [
+              ///Image
+              Image(
+                image: AssetImage(image),
+                width: THelperFunctions.screenWidth() * 0.6,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+
+              /// Title & SubTitle
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwItems,
+              ),
+              Text(
+                subTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+
+              /// Buttons
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onPressed,
+                  child: const Text(TTexts.tContinue),
+                ),
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwItems,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
