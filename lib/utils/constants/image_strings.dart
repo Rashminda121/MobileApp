c4c943fafeb28bfdd07e698b3ba61027376aacc0
Rashminda <rashminda121@gmail.com>
2024-04-08 00:14:36 +0530

    Update image_strings.dart

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -10,27 +10,47 @@ class TImages {
   //onboarding
   static const String onboard1 = "assets/images/on_boarding_images/w1.gif";
   static const String onboard2 = "assets/images/on_boarding_images/w2.jpg";
+  static const String hello = "assets/images/animations/sammy-line-lettering-hello.png";
+  static const String deliver = "assets/images/animations/sammy-line-man-and-dog-delivering.gif";
+  static const String payment = "assets/images/animations/sammy-line-placing-order-on-smartphone.gif";
 
-  static const String onboard3 = "assets/images/on_boarding_images/w1.gif";
-  static const String productImage1 = "assets/logos/facebook.png";
-
-  static const String hello =
-      "assets/images/animations/sammy-line-lettering-hello.png";
-  static const String deliver =
-      "assets/images/animations/sammy-line-man-and-dog-delivering.gif";
-  static const String payment =
-      "assets/images/animations/sammy-line-placing-order-on-smartphone.gif";
 
   //animations
-  static const String deliverdInPlane =
-      "assets/images/animations/sammy-line-come-back-later.png";
-  static const String productsale =
-      "assets/images/animations/sammy-line-lettering-flash-sale-with-a-man-carrying-gift-boxes-text.png";
-  static const String staticSuccess =
-      "assets/images/animations/sammy-line-man-received-a-certificate.png";
-  static const String verify =
-      "assets/images/animations/sammy-line-travel-backpack-with-passport-and-air-ticket.gif";
-  static const String confirmation =
-      "assets/images/animations/sammy-line-man-receives-a-mail.png";
+  static const String deliverdInPlane = "assets/images/animations/sammy-line-come-back-later.png";
+  static const String productsale = "assets/images/animations/sammy-line-lettering-flash-sale-with-a-man-carrying-gift-boxes-text.png";
+  static const String staticSuccess = "assets/images/animations/sammy-line-man-received-a-certificate.png";
+  static const String verify = "assets/images/animations/sammy-line-travel-backpack-with-passport-and-air-ticket.gif";
+  static const String deliverEmail = "assets/images/animations/sammy-line-man-receives-a-mail.png";
+  static const String paymentSuccess = "assets/images/animations/icons8-correct.gif";
+
+
+  //Category Icons
+  static const String shoeIcon = "assets/icons/categories/sport-shoe.png";
+
+  //banner images
+  static const String promobanner= "assets/images/banners/banner.jpg";
+  static const String promobanner2= "assets/images/banners/banner2.jpg";
+  static const String promobanner3= "assets/images/banners/banner3.jpg";
+
+  //product images
+  static const String product1= "assets/images/products/sport-shoe.png";
+  static const String product2= "assets/images/products/shoe.jpg";
+
+
+  //user images
+  static const String user= "assets/images/user/profile-user.png";
+
+  //product reviews
+  static const String profile1= "assets/images/reviews/profile1.png";
+  static const String profile2= "assets/images/reviews/profile2.png";
+  static const String profile3= "assets/images/reviews/profile3.png";
+
+  //payment
+  static const String paypal= "assets/images/payment/paypal.png";
+  static const String visa= "assets/images/payment/visa.png";
+  static const String masterCard= "assets/images/payment/mastercard.jpeg";
+  static const String creditCard= "assets/images/payment/creditcard.jpeg";
+  static const String applePay= "assets/images/payment/applepay.jpeg";
+  static const String gPay= "assets/images/payment/gpay.jpeg";
 
 }
