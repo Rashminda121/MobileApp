20a1e11b5a00e461a07ec4f00db8d5376ece05bd
Rashminda <rashminda121@gmail.com>
2024-04-08 00:09:53 +0530

    Update main.dart
    Co-Authored-By: Onaliy Vinukiy Jayawardana <114973581+OnaliyVinukiy@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-
 import 'package:flutter/material.dart';
-import 'package:mobile_app/utils/theme/theme.dart';
-import 'package:mobile_app/utils/constants/colors.dart';
+
 import 'app.dart';
 
 void main() {
