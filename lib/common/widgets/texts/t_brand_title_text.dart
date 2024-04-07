85683a6994910c0e8c6de6cd9790e21d63ed8333
Rashminda <rashminda121@gmail.com>
2024-04-08 00:12:32 +0530

    Create t_brand_title_text.dart

---
+++ lib/common/widgets/texts/t_brand_title_text.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/enums.dart';
+
+class TBrandTitleText extends StatelessWidget {
+  const TBrandTitleText({
+    super.key,
+    this.color,
+    this.maxLines = 1,
+    required this.title,
+    this.textAlign = TextAlign.center,
+    this.brandTextSize = TextSizes.small,
+  });
+
+  final Color? color;
+  final String title;
+  final int maxLines;
+  final TextAlign? textAlign;
+  final TextSizes brandTextSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      textAlign: textAlign,
+      maxLines: maxLines,
+      overflow: TextOverflow.ellipsis,
+      style: brandTextSize == TextSizes.small
+          ? Theme.of(context).textTheme.labelMedium!.apply(color: color)
+          : brandTextSize == TextSizes.medium
+          ? Theme.of(context).textTheme.bodyLarge!.apply(color: color)
+          : brandTextSize == TextSizes.large
+          ? Theme.of(context).textTheme.titleLarge!.apply(color: color)
+          : Theme.of(context).textTheme.bodyMedium!.apply(color: color),
+    );
+  }
+}
