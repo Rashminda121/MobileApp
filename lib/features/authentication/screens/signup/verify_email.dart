c8a889bab1c7ce3e5e267b28e363783c2a95a854
Rashminda <rashminda121@gmail.com>
2024-04-08 00:12:53 +0530

    Create verify_email.dart

---
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -0,0 +1,93 @@
+
+import "package:flutter/cupertino.dart";
+import "package:flutter/material.dart";
+import "package:get/get.dart";
+
+import "../../../../common/widgets/success_screen/success_screen.dart";
+import "../../../../utils/constants/image_strings.dart";
+import "../../../../utils/constants/sizes.dart";
+import "../../../../utils/constants/text_strings.dart";
+import "../../../../utils/helpers/helper_functions.dart";
+import "../login/login.dart";
+
+class VerifyEmailScreen extends StatelessWidget {
+  const VerifyEmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+              onPressed: () => Get.offAll(() => const LoginScreen()),
+              icon: const Icon(CupertinoIcons.clear)),
+        ],
+      ),
+      body: SingleChildScrollView(
+        // Padding to Give Default Equal Space on all sides in all screens.
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Image
+              Image(
+                image: const AssetImage(TImages.deliverEmail),
+                width: THelperFunctions.screenWidth() * 0.6,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              /// Title & SubTitle
+              Text(
+                TTexts.confirmEmail,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              Text(
+                'ecommerce@gmail.com',
+                style: Theme.of(context).textTheme.labelLarge,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwItems,
+              ),
+              Text(
+                TTexts.confirmEmailSubTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+
+              /// Buttons
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => Get.to(() =>  SuccessScreen(
+                    image: TImages.staticSuccess,
+                    title: TTexts.yourAccountCreatedTitle,
+                    subTitle: TTexts.yourAccountCreatedSubTitle,
+                    onPressed:() => Get.to(() => const LoginScreen()),
+                  ),),
+                  child: const Text(TTexts.tContinue),
+                ),
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwItems,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(TTexts.resendEmail),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
