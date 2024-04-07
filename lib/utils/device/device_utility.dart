a95b1d2d6c3997c6ccc1c35fe4b5e504097a6a2a
Rashminda <rashminda121@gmail.com>
2024-04-08 00:14:40 +0530

    Update device_utility.dart

--- lib/utils/device/device_utility.dart
+++ lib/utils/device/device_utility.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 
