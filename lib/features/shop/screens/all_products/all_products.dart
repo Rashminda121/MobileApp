9dc721777e70ea7765f1c4eb38940ba8f600c8ad
Rashminda <rashminda121@gmail.com>
2024-04-08 00:13:22 +0530

    Create all_products.dart

---
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../../common/widgets/appbar/appbar.dart';
+import '../../../../common/widgets/products/sortable/sortable_products.dart';
+import '../../../../utils/constants/sizes.dart';
+
+class AllProducts extends StatelessWidget {
+  const AllProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: TAppBar(title: Text('Popular Products'), showBackArrow: true),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: TSortableProducts(),
+        ),
+      ),
+    );
+  }
+}
+
+
