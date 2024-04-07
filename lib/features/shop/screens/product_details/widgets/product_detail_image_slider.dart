e3290c3e33e99d48379db61ac9cfa6d75c984912
Rashminda <rashminda121@gmail.com>
2024-04-08 00:14:05 +0530

    Create product_detail_image_slider.dart

---
+++ lib/features/shop/screens/product_details/widgets/product_detail_image_slider.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../../common/widgets/appbar/appbar.dart';
+import '../../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import '../../../../../common/widgets/icons/t_circular_icon.dart';
+import '../../../../../common/widgets/images/t_rounded_image.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/image_strings.dart';
+import '../../../../../utils/constants/sizes.dart';
+import '../../../../../utils/helpers/helper_functions.dart';
+
+class TProductImageSlider extends StatelessWidget {
+  const TProductImageSlider({
+    super.key,
+  });
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return TCurvedEdgesWidget(
+      child: Container(
+        color: dark ? TColors.darkerGrey : TColors.light,
+        child: Stack(
+          children: [
+            /// Main Large Image
+            const SizedBox(
+              height: 400,
+              child: Padding(
+                padding: EdgeInsets.all(TSizes.productImageRadius * 2),
+                child: Center(
+                    child: Image(image: AssetImage(TImages.product1))),
+              ),
+            ),
+
+            /// image slider
+            Positioned(
+              right: 0,
+              bottom: 30,
+              left: TSizes.defaultSpace,
+              child: SizedBox(
+                height: 80,
+                child: ListView.separated(
+                  itemCount: 6,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  itemBuilder: (_, index) => TRoundedImage(
+                    width: 80,
+                    backgroundColor:
+                    dark ? TColors.dark : TColors.white,
+                    border: Border.all(color: TColors.primary),
+                    padding: const EdgeInsets.all(TSizes.sm),
+                    imageUrl: TImages.product1,
+                  ),
+                  separatorBuilder: (_, __) =>
+                  const SizedBox(width: TSizes.spaceBtwItems),
+                ),
+              ),
+            ),
+
+            ///App Bar Icons
+            const TAppBar(
+              showBackArrow: true,
+              actions: [
+                TCircularIcon(
+                  icon: Iconsax.heart5,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+
+
+
+
+          ], // TCurvedEdgesWidget
+        ),
+      ),
+    );
+  }
+}
