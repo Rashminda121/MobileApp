f01d336c2067fcfee614f4c36fb46c5c4b9b9df2
Rashminda <rashminda121@gmail.com>
2024-04-08 00:13:37 +0530

    Create billing_payment_section.dart

---
+++ lib/features/shop/screens/checkout/widgets/billing_payment_section.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../../../common/styles/rounded_container.dart';
+import '../../../../../common/widgets/texts/section_heading.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/image_strings.dart';
+import '../../../../../utils/constants/sizes.dart';
+import '../../../../../utils/helpers/helper_functions.dart';
+
+class TBillingPaymentSection extends StatelessWidget {
+  const TBillingPaymentSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final dark = THelperFunctions.isDarkMode(context);
+    return Column(
+      children: [
+        TSectionHeading(
+          title: 'Payment Method',
+          buttonTitle: 'Change',
+          onPressed: (){},
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems/2),
+        Row(
+          children: [
+            TRoundedContainer(
+              width: 60,
+              height: 35,
+              backgroundColor: dark ? TColors.light : TColors.white,
+              padding: const EdgeInsets.all(TSizes.sm),
+              child: const Image(
+                image: AssetImage(TImages.paypal),
+                fit: BoxFit.contain,
+              ),
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems/2),
+            Text('Paypal',style: Theme.of(context).textTheme.bodyLarge),
+          ],
+        )
+      ],
+    );
+  }
+}
