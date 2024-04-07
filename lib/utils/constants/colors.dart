ce55eec0b2ca825cd6c4f40c319b8122f53bc4e3
Rashminda <rashminda121@gmail.com>
2024-04-08 00:14:35 +0530

    Update colors.dart

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -4,7 +4,7 @@ class TColors{
   TColors._();
 
   //App Basic Color
-  static const Color primaryColor = Color(0xFF4B68FF);
+  static const Color primary = Color(0xFF4B68FF);
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
 
@@ -20,7 +20,7 @@ class TColors{
 
   //Background Colors
   static const Color light = Color(0xFFF6F6F6);
-  static const Color dark = Color(0xff2727227);
+  static const Color dark = Color(0xdd272722);
   static const Color primaryBackground= Color(0xFFF3F5FF);
 
   //Background Container Colors
