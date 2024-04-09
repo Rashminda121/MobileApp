c4ba0dc21b5173b0eb4255adf44618438b58ec7a
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-04-09 16:31:30 +0530

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:mobile_app/data/repositories.authentication/authentication_repository.dart';
+import 'package:mobile_app/firebase_options.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'firebase_options.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 
-void main() {
+/// Entry point of the application
+Future <void> main() async {
+
+///widgets binding
+final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+await GetStorage.init();
+FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+ 
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform).then ((FirebaseApp value) => Get.put(AuthenticationRepository()),
+
+    );
   runApp(const App());
 }
 
