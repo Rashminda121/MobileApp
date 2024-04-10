4a30700d6ce715fca2801179edee6c0dade47c5e
Avish Madushanka <avishmadushanka21@gmail.com>
2024-04-10 19:24:29 +0530

    Update login_controller.dart

--- lib/features/authentication/controllers/login/login_controller.dart
+++ lib/features/authentication/controllers/login/login_controller.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
+class LoginController extends GetxController {
+  final rememberMe = false.obs;
+  final hidePassword = true.obs;
+  final localStorage = GetStorage();
+  final email = TextEditingController();
+  final password = TextEditingController();
+  
+  GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+  
+}
