543dd2ac71e1aaaab97147a01dbb37d372147194
Jayamuni Rashminda <rashminda121@gmail.com>
2024-04-10 16:14:12 +0530

    Update vertical_image_text.dart

--- lib/common/widgets/image_text_widgets/vertical_image_text.dart
+++ lib/common/widgets/image_text_widgets/vertical_image_text.dart
@@ -9,19 +9,21 @@ class TVerticalImageText extends StatelessWidget {
     super.key,
     required this.image,
     required this.title,
-    this.textColor=TColors.white,
+    this.textColor = TColors.white,
     this.backgroundColor,
     this.onTap,
+    this.isNetWorkImage = true,
   });
 
   final String image, title;
   final Color textColor;
   final Color? backgroundColor;
   final void Function()? onTap;
+  final bool isNetWorkImage;
 
   @override
   Widget build(BuildContext context) {
-    final dark=THelperFunctions.isDarkMode(context);
+    final dark = THelperFunctions.isDarkMode(context);
     return GestureDetector(
       onTap: onTap,
       child: Padding(
@@ -34,7 +36,8 @@ class TVerticalImageText extends StatelessWidget {
                 height: 56,
                 padding: const EdgeInsets.all(TSizes.sm),
                 decoration: BoxDecoration(
-                  color: backgroundColor??(dark ? TColors.black  : TColors.white),
+                  color:
+                      backgroundColor ?? (dark ? TColors.black : TColors.white),
                   borderRadius: BorderRadius.circular(100),
                 ),
                 child: Center(
