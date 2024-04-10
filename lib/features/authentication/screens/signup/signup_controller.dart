286da0e6750dadd20dc6076c80a7dc67137f6be5
avish madushanka <avishmadushanka21@gmail.com>
2024-04-10 09:30:38 +0530

    check internet connection

--- lib/features/authentication/screens/signup/signup_controller.dart
+++ lib/features/authentication/screens/signup/signup_controller.dart
@@ -9,46 +9,51 @@ class SignupController extends GetxController {
   static SignupController get instance => Get.find();
   SignupController();
 
-    ///Variables
-    final hidePassword = true.obs;
-    final privacyPolicy = true.obs;
-    final email = TextEditingController();
-    final lastName = TextEditingController();
-    final username = TextEditingController();
-    final password = TextEditingController();
-    final firstName = TextEditingController();
-    final phoneNumber = TextEditingController();
-    GlobalKey <FormState> signupFormKey = GlobalKey<FormState>();
+  ///Variables
+  final hidePassword = true.obs;
+  final privacyPolicy = true.obs;
+  final email = TextEditingController();
+  final lastName = TextEditingController();
+  final username = TextEditingController();
+  final password = TextEditingController();
+  final firstName = TextEditingController();
+  final phoneNumber = TextEditingController();
+  GlobalKey<FormState> signupFormKey = GlobalKey<FormState>();
 
   Future<void> signup() async {
     try {
-  // Start Loading
-    TFullScreenLoader.openLoadingDialog('We are processing your information...', TImages.docerAnimation);
+      // Start Loading
+      TFullScreenLoader.openLoadingDialog(
+          'We are processing your information...', TImages.docerAnimation);
 
-  // Check Internet Connectivity
-  final isConnected = await NetworkManager.instance.isConnected();
-  if (!isConnected) return;
-  
-
-
-  // Form Validation
-  if (!signupFormKey.currentState!.validate())return;
-  
-
-  // Privacy Policy Check
-
-
-  // Register user in the Firebase Authentication & Save user data in the Firebase
- 
-  // Save Authenticated user data in the Firebase Firestore
-
-  // Show Success Message
-  // Move to Verify Email Screen
-      } catch (e) {
-        TLoaders.errorSnackBar(title: 'Oh Snap', message: e.toString());
-        // Show some Generic Error to the user
-      } finally {
+      // Check Internet Connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        //Remove Loader
         TFullScreenLoader.stopLoading();
+        return;
+      }
+
+      // Form Validation
+      if (!signupFormKey.currentState!.validate()) {
+        //Remove Loader
+        TFullScreenLoader
+        return;
+      }
+
+      // Privacy Policy Check
+
+      // Register user in the Firebase Authentication & Save user data in the Firebase
+
+      // Save Authenticated user data in the Firebase Firestore
+
+      // Show Success Message
+      // Move to Verify Email Screen
+    } catch (e) {
+      TLoaders.errorSnackBar(title: 'Oh Snap', message: e.toString());
+      // Show some Generic Error to the user
+    } finally {
+      TFullScreenLoader.stopLoading();
     }
   }
 }
