55657aec0031bd3ac2a0747f3ddf677eb742dd44
Jayamuni Rashminda <rashminda121@gmail.com>
2024-04-10 17:30:29 +0530

    Update dummy_data.dart

--- lib/common/widgets/shimmers/dummy_data.dart
+++ lib/common/widgets/shimmers/dummy_data.dart
@@ -1,68 +1,63 @@
-// import 'package:flutter/material.dart';
-//
-// import '../../../features/shop/models/category_model.dart';
-// import '../../../utils/constants/image_strings.dart';
-//
-// class TDummyData {
-//   ///
-//   static final List<BannerModel> banners = [
-//     BannerModel(imageUrl: TImages.banner1, targetScreen: TRoutes.order, active: false),
-//     BannerModel(imageUrl: TImages.banner2, targetScreen: TRoutes.cart, active: true),
-//     BannerModel(
-//         imageUrl: TImages.banner3,
-//         targetScreen: TRoutes.favourites,
-//         active: true),
-//     BannerModel(imageUrl: TImages.banner4, targetScreen: TRoutes.search, active: true),
-//     BannerModel(imageUrl: TImages.banner5, targetScreen: TRoutes.settings, active: true),
-//     BannerModel(
-//         imageUrl: TImages.banner6,
-//         targetScreen: TRoutes.userAddress,
-//         active: true),
-//     BannerModel(imageUrl: TImages.banner8, targetScreen: TRoutes.checkout, active: false),
-//   ];
-//
-//   ///user
-//   static final UserModel user = UserModel(
-//     firstName: 'Coding',
-//     lastName: 'with T',
-//     email: 'support@codingwithT.com',
-//     phoneNumber: '+14155552671',
-//     profilePicture: TImages.user,
-//     addresses: [
-//       AddressModel(
-//         id: '1',
-//         name: 'Coding with T',
-//         phoneNumber: '+923178059528',
-//         street: '82356 Timmy Coves',
-//         city: 'South Liana',
-//         state: 'Maine',
-//         postalCode: '87665',
-//         country: 'USA',
-//       ),
-//     ],
-//   );
-//
-//   ///list of all categories
-//   static final List<CategoryModel> categories = [
-//     CategoryModel(id: '1', image: TImages.sportIcon, name: 'Sports', isFeatured: true),
-//     CategoryModel(id: '5', image: TImages.furnitureIcon, name: 'Furniture', isFeatured: true),
-//     CategoryModel(id: '2', image: TImages.electronicsIcon, name: 'Electronics', isFeatured: true),
-//     CategoryModel(id: '3', image: TImages.clothIcon, name: 'Clothes', isFeatured: true),
-//     CategoryModel(id: '4', image: TImages.animalIcon, name: 'Animals', isFeatured: true),
-//     CategoryModel(id: '6', image: TImages.shoeIcon, name: 'Shoes', isFeatured: true),
-//     CategoryModel(id: '7', image: TImages.cosmeticsIcon, name: 'Cosmetics', isFeatured: true),
-//     CategoryModel(id: '14', image: TImages.jeweleryIcon, name: 'Jewelery', isFeatured: true),
-//     CategoryModel(id: '8', image: TImages.sportIcon, name: 'Sport Shoes', parentId: '1', isFeatured: false),
-//     CategoryModel(id: '9', image: TImages.sportIcon, name: 'Track suits', parentId: '1', isFeatured: false),
-//     CategoryModel(id: '10', image: TImages.sportIcon, name: 'Sports Equipments', parentId: '1', isFeatured: false),
-//     CategoryModel(id: '11', image: TImages.furnitureIcon, name: 'Bedroom furniture', parentId: '5', isFeatured: false),
-//     CategoryModel(id: '12', image: TImages.furnitureIcon, name: 'Kitchen furniture', parentId: '5', isFeatured: false),
-//     CategoryModel(id: '13', image: TImages.furnitureIcon, name: 'Office furniture', parentId: '5', isFeatured: false),
-//     CategoryModel(id: '14', image: TImages.electronicsIcon, name: 'Laptop', parentId: '2', isFeatured: false),
-//     CategoryModel(id: '15', image: TImages.electronicsIcon, name: 'Mobile', parentId: '2', isFeatured: false),
-//   ];
-//
-//
-//
-//
-// }
+import 'package:flutter/material.dart';
+
+import '../../../features/shop/models/category_model.dart';
+import '../../../utils/constants/image_strings.dart';
+
+class TDummyData {
+  ///
+  /// -- Banners
+  // static final List<BannerModel> banners = [
+  //   BannerModel(imageUrl: TImages.banner1, targetScreen: TRoutes.order, active: false),
+  //   BannerModel(imageUrl: TImages.banner2, targetScreen: TRoutes.cart, active: true),
+  //   BannerModel(imageUrl: TImages.banner3, targetScreen: TRoutes.favourites, active: true),
+  //   BannerModel(imageUrl: TImages.banner4, targetScreen: TRoutes.search, active: true),
+  //   BannerModel(imageUrl: TImages.banner5, targetScreen: TRoutes.settings, active: true),
+  //   BannerModel(imageUrl: TImages.banner6, targetScreen: TRoutes.userAddress, active: true),
+  //   BannerModel(imageUrl: TImages.banner8, targetScreen: TRoutes.checkout, active: false),
+  // ];
+
+  ///user
+  // static final UserModel user = UserModel(
+  //   firstName: 'Coding',
+  //   lastName: 'with T',
+  //   email: 'support@codingwithT.com',
+  //   phoneNumber: '+14155552671',
+  //   profilePicture: TImages.user,
+  //   addresses: [
+  //     AddressModel(
+  //       id: '1',
+  //       name: 'Coding with T',
+  //       phoneNumber: '+923178059528',
+  //       street: '82356 Timmy Coves',
+  //       city: 'South Liana',
+  //       state: 'Maine',
+  //       postalCode: '87665',
+  //       country: 'USA',
+  //     ),
+  //   ],
+  // );
+
+  // ///list of all categories
+  // static final List<CategoryModel> categories = [
+  //   CategoryModel(id: '1', image: TImages.sportIcon, name: 'Sports', isFeatured: true),
+  //   CategoryModel(id: '5', image: TImages.furnitureIcon, name: 'Furniture', isFeatured: true),
+  //   CategoryModel(id: '2', image: TImages.electronicsIcon, name: 'Electronics', isFeatured: true),
+  //   CategoryModel(id: '3', image: TImages.clothIcon, name: 'Clothes', isFeatured: true),
+  //   CategoryModel(id: '4', image: TImages.animalIcon, name: 'Animals', isFeatured: true),
+  //   CategoryModel(id: '6', image: TImages.shoeIcon, name: 'Shoes', isFeatured: true),
+  //   CategoryModel(id: '7', image: TImages.cosmeticsIcon, name: 'Cosmetics', isFeatured: true),
+  //   CategoryModel(id: '14', image: TImages.jeweleryIcon, name: 'Jewelery', isFeatured: true),
+  //   CategoryModel(id: '8', image: TImages.sportIcon, name: 'Sport Shoes', parentId: '1', isFeatured: false),
+  //   CategoryModel(id: '9', image: TImages.sportIcon, name: 'Track suits', parentId: '1', isFeatured: false),
+  //   CategoryModel(id: '10', image: TImages.sportIcon, name: 'Sports Equipments', parentId: '1', isFeatured: false),
+  //   CategoryModel(id: '11', image: TImages.furnitureIcon, name: 'Bedroom furniture', parentId: '5', isFeatured: false),
+  //   CategoryModel(id: '12', image: TImages.furnitureIcon, name: 'Kitchen furniture', parentId: '5', isFeatured: false),
+  //   CategoryModel(id: '13', image: TImages.furnitureIcon, name: 'Office furniture', parentId: '5', isFeatured: false),
+  //   CategoryModel(id: '14', image: TImages.electronicsIcon, name: 'Laptop', parentId: '2', isFeatured: false),
+  //   CategoryModel(id: '15', image: TImages.electronicsIcon, name: 'Mobile', parentId: '2', isFeatured: false),
+  // ];
+
+
+
+
+}
