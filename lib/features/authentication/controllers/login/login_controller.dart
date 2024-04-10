057724c7b8ce48f911c609e47fc213a14a2ba65b
Avish Madushanka <avishmadushanka21@gmail.com>
2024-04-10 19:58:49 +0530

    Update login_controller.dart

--- lib/features/authentication/controllers/login/login_controller.dart
+++ lib/features/authentication/controllers/login/login_controller.dart
@@ -2,14 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:mobile_app/utils/constants/image_strings.dart';
+import 'package:mobile_app/utils/helpers/network_manager.dart';
+import 'package:mobile_app/utils/popups/full_screen_loader.dart';
 
 class LoginController extends GetxController {
+  /// Variables
   final rememberMe = false.obs;
   final hidePassword = true.obs;
   final localStorage = GetStorage();
   final email = TextEditingController();
   final password = TextEditingController();
-  
+
   GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
-  
+
+  ///-- Email and Password SignIn
+  Future<void> emailAndPasswordSignIn() async {
+    try {
+      // Start Loading
+      TFullScreenLoader.openLoadingDialog('Logging you in...', TImages.docerAnimation);
+      // Check Internet Connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        TFullScreenLoader.stopLoading();
+      return;
+      }
+      
+      // Form Validation
+      if (!loginFormKey.currentState!.validate()) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+      // Save Data if Remember Me is selected
+      if (rememberMe.value) {
+        // TODO: Save data in local storage
+      }
+    } catch (e) {
+      // TODO: Handle error
+    }
+  }
 }
