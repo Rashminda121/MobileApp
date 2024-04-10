8fa954a797c0f5fc70d786eb4f424a51cc074411
Avish Madushanka <avishmadushanka21@gmail.com>
2024-04-10 15:04:09 +0530

    Update verify_email_controller.dart

--- lib/features/authentication/screens/signup/verify_email_controller.dart
+++ lib/features/authentication/screens/signup/verify_email_controller.dart
@@ -1,17 +1,28 @@
-import 'package: get/get.dart';
+import 'package:get/get.dart';
+import 'package:mobile_app/data/repositories.authentication/authentication/authentication_repository.dart';
+
+import '../../../../utils/popups/loaders.dart';
 
 class VerifyEmailController extends GetxController {
   static VerifyEmailController get instance => Get.find();
 
-  //Send Email whenever verify screen appearrs & set timer for auto redirect.
+  /// Send Email Whenever Verify Screen appears & Set Timer for auto redirect.
   @override
   void onInit() {
+    sendEmailVerification();
     super.onInit();
   }
 
-  // Send Email Verification Link
+  /// Send Email Verification link
+  sendEmailVerification() async {
+    try {
+      await AuthenticationRepository.instance.sendEmailVerification();
+      TLoaders.successSnackBar(title: 'Email Sent', message: 'Please Check your inbox and verify your email.');
+    } catch (e) {
+      TLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
+    }
 
-  // Timer to automotically redirect on email verifyication
-
-  //Manually check if email verified
+    /// Timer to automatically redirect on Email Verification
+    /// Manually Check if Email Verified
+  }
 }
