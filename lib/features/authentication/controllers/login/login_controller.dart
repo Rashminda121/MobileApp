b1e49c5297b5cb8999e4986e53aa9696d05bb4b1
Avish Madushanka <avishmadushanka21@gmail.com>
2024-04-10 20:03:32 +0530

    Update login_controller.dart

--- lib/features/authentication/controllers/login/login_controller.dart
+++ lib/features/authentication/controllers/login/login_controller.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:mobile_app/data/repositories.authentication/authentication/authentication_repository.dart';
 import 'package:mobile_app/utils/constants/image_strings.dart';
 import 'package:mobile_app/utils/helpers/network_manager.dart';
 import 'package:mobile_app/utils/popups/full_screen_loader.dart';
+import 'package:mobile_app/utils/popups/loaders.dart';
 
 class LoginController extends GetxController {
   /// Variables
@@ -18,27 +20,50 @@ class LoginController extends GetxController {
 
   ///-- Email and Password SignIn
   Future<void> emailAndPasswordSignIn() async {
-    try {
-      // Start Loading
-      TFullScreenLoader.openLoadingDialog('Logging you in...', TImages.docerAnimation);
-      // Check Internet Connectivity
-      final isConnected = await NetworkManager.instance.isConnected();
-      if (!isConnected) {
-        TFullScreenLoader.stopLoading();
-      return;
-      }
-      
-      // Form Validation
-      if (!loginFormKey.currentState!.validate()) {
-        TFullScreenLoader.stopLoading();
-        return;
-      }
-      // Save Data if Remember Me is selected
-      if (rememberMe.value) {
-        // TODO: Save data in local storage
-      }
-    } catch (e) {
-      // TODO: Handle error
-    }
+  // Start Loading
+  try{
+  TFullScreenLoader.openLoadingDialog('Logging you in...', TImages.docerAnimation);
+
+  // Check Internet Connectivity
+  final isConnected = await NetworkManager.instance.isConnected();
+  if (!isConnected) {
+    TFullScreenLoader.stopLoading();
+    return;
+  }
+
+  // Form Validation
+  if (!loginFormKey.currentState!.validate()) {
+    TFullScreenLoader.stopLoading();
+    return;
+  }
+
+  // Save Data if Remember Me is selected
+  if (rememberMe.value) {
+    localStorage.write('REMEMBER_ME_EMAIL', email.text.trim());
+    localStorage.write('REMEMBER_ME_PASSWORD', password.text.trim());
+  }
+
+  // Login user using EMail & Password Authentication
+  final userCredentials = await AuthenticationRepository.instance.loginWithEmailAndPassword(email.text.trim(), password.text.trim());
+
+  // Remove Loader
+  TFullScreenLoader.stopLoading();
+
+  // Redirect
+  AuthenticationRepository.instance.screenRedirect();
+} catch (e){
+TFullScreenLoader.stopLoading();
+TLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
+}
+}
+
+  ///-- Remember Me
+  void rememberMeFunction() {
+    rememberMe.value = !rememberMe.value;
+  }
+
+  ///-- Show Password
+  void showPassword() {
+    hidePassword.value = !hidePassword.value;
   }
 }
