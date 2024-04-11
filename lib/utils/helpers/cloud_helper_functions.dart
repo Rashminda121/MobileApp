2e6838450593804141aed56f853198548c31efb3
Onaliy Vinukiy Jayawardana <ovjayawardana@students.nsbm.ac.lk>
2024-04-11 23:17:42 +0530

    Create cloud_helper_functions.dart

---
+++ lib/utils/helpers/cloud_helper_functions.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+class TCloudHelperFunctions {
+  /// Helper function to check the state of a single database record.
+  ///
+  /// Returns a Widget based on the state of the snapshot.
+  /// If data is still loading, it returns a Circular Progress Indicator.
+  /// If no data is found, it returns a generic "No Data Found" message.
+  /// If an error occurs, it returns a generic error message.
+  /// Otherwise, it returns null.
+  static Widget? checkSingleRecordState<T>(AsyncSnapshot<T> snapshot) {
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    if (!snapshot.hasData || snapshot.data == null) {
+      return const Center(child: Text('No Data Found!'));
+    }
+    if (snapshot.hasError) {
+      return const Center(child: Text('Something went wrong.'));
+    }
+    return null;
+  }
+
+
+
+
+  static Widget? checkMultiRecordState<T>({
+    required AsyncSnapshot<List<T>> snapshot,
+    Widget? loader,
+    Widget? error,
+    Widget? nothingFound,
+  }) {
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      if (loader != null) return loader;
+      return const Center(child: CircularProgressIndicator());
+    }
+    if (!snapshot.hasData || snapshot.data == null || snapshot.data!.isEmpty) {
+      if (nothingFound != null) return nothingFound;
+      return const Center(child: Text('No Data Found!'));
+    }
+    if (snapshot.hasError) {
+      if (error != null) return error;
+      return const Center(child: Text('Something went wrong.'));
+    }
+    return null;
+  }
+
+  static Future<String> getURLFromFilePathAndName(String path) async {
+    try {
+      if (path.isEmpty) return '';
+      final ref = FirebaseStorage.instance.ref().child(path);
+      final url = await ref.getDownloadURL();
+      return url;
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } on PlatformException catch (e) {
+      throw e.message!;
+    } catch (e) {
+      throw 'something went wrong';
+    }
+  }
+}
