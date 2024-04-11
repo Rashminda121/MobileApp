31b36f5b7f28f058c378e4c92b6393b408473663
Jayamuni Rashminda <rashminda121@gmail.com>
2024-04-11 09:42:19 +0530

    46

--- lib/common/widgets/products/sortable/sortable_products.dart
+++ lib/common/widgets/products/sortable/sortable_products.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:mobile_app/features/shop/controllers/product/all_product_controller.dart';
+import 'package:mobile_app/features/shop/models/produt_model.dart';
 
 import '../../../../utils/constants/sizes.dart';
 import '../../layouts/grid_layout.dart';
@@ -7,24 +11,33 @@ import '../product_cards/product_card_vertical.dart';
 
 class TSortableProducts extends StatelessWidget {
   const TSortableProducts({
-    super.key,
+    super.key, required this.products,
   });
+
+  final List<ProductModel> products;
+
 
   @override
   Widget build(BuildContext context) {
+
+    final controller =Get.put(AllProductsController());
+    controller.assignProducts(products);
     return Column(
       children: [
         /// Dropdown
         DropdownButtonFormField(
           decoration: const InputDecoration(prefixIcon: Icon(Iconsax.sort)),
-          onChanged: (value) {},
+          value: controller.selectedSortOption.value,
+          onChanged: (value) {
+            controller.sortProducts(value!);
+          },
           items: ['Name','Higher Price','Lower Price','Sale','Newest','Popularity'].map((option) =>DropdownMenuItem(value:option,child: Text(option))).toList(),
         ),
         const SizedBox(height: TSizes.spaceBtwSections),
 
         ///products
 
-        TGridLayout(itemCount: 8, itemBuilder: (_,index)=>const TProductCardVertical()),
+        Obx(()=> TGridLayout(itemCount: controller.products.length, itemBuilder: (_,index)=>TProductCardVertical(product: controller.products[index],))),
 
 
       ],
