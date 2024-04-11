ee99e498d8e38a5d39f326f28a228da07335e2a5
Jayamuni Rashminda <rashminda121@gmail.com>
2024-04-11 09:42:23 +0530

    Update product_repository.dart

--- lib/data/repositories.authentication/product/product_repository.dart
+++ lib/data/repositories.authentication/product/product_repository.dart
@@ -15,17 +15,45 @@ class ProductRepository extends GetxController {
   /// Get limited featured products
   Future<List<ProductModel>> getFeaturedProducts() async {
     try {
-      // Add your Firestore query here
-      final snapshot = await _db.collection('products').where('IsFeatured',isEqualTo:true).limit(4).get();
-
+      final snapshot = await _db.collection('Products').where('IsFeatured', isEqualTo: true).limit(4).get();
+      return snapshot.docs.map((e) => ProductModel.fromSnapshot(e)).toList();
     } on FirebaseException catch (e) {
       throw TFirebaseException(e.code).message;
     } on PlatformException catch (e) {
       throw TPlatformException(e.code).message;
     } catch (e) {
-      throw 'Something went wrong. Please try again';
     }
+    throw 'Something went wrong. Please try again';
   }
+
+  /// Get limited featured products
+  Future<List<ProductModel>> getAllFeaturedProducts() async {
+    try {
+      final snapshot = await _db.collection('Products').where('IsFeatured', isEqualTo: true).get();
+      return snapshot.docs.map((e) => ProductModel.fromSnapshot(e)).toList();
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+    }
+    throw 'Something went wrong. Please try again';
+  }
+
+  Future<List<ProductModel>> fetchProductsByQuery(Query query) async {
+    try {
+      final querySnapshot = await query.get();
+      final List<ProductModel> productList = querySnapshot.docs.map((doc) => ProductModel.fromQuerySnapshot(doc)).toList();
+      return productList;
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+    }
+    throw 'Something went wrong. Please try again';
+  }
+
 
   /// Upload dummy data to the Cloud Firebase
   Future<void> uploadDummyData(List<ProductModel> products) async {
