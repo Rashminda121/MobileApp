81c8ee668cabb204efed670183912bd520c5b066
Onaliy Vinukiy Jayawardana <ovjayawardana@students.nsbm.ac.lk>
2024-04-11 23:21:50 +0530

    Create product_controller.dart

---
+++ lib/features/shop/controllers/product_controller.dart
@@ -0,0 +1,101 @@
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:mobile_app/utils/constants/enums.dart';
+import 'package:mobile_app/utils/popups/loaders.dart';
+
+import '../../../data/repositories.authentication/product/product_repository.dart';
+import '../models/product_model.dart';
+
+class ProductController extends GetxController {
+  static ProductController get instance => Get.find();
+
+  final productRepository= Get.put(ProductRepository());
+  final isLoading = false.obs;
+  RxList<ProductModel> featuredProducts = <ProductModel>[].obs;
+
+  @override
+  void onInit() {
+    fetchFeaturedProducts();
+    super.onInit();
+  }
+
+  void fetchFeaturedProducts() async {
+    try {
+      // Show loader while loading Products
+      isLoading.value = true;
+
+      //fetch products
+      final products= await productRepository.getFeaturedProducts();
+
+      //Asiign products
+      featuredProducts.assignAll(products);
+
+    } catch (e) {
+      TLoaders.errorSnackBar(title: 'Oh Snap',message: e.toString());
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+
+  Future<List<ProductModel>> fetchAllFeaturedProducts() async {
+    try {
+      // Fetch Products
+      final products = await productRepository.getFeaturedProducts();
+      return products;
+    } catch (e) {
+      TLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
+      return [];
+    }
+  }
+
+  String getProductPrice(ProductModel product) {
+    double smallPrice = double.infinity;
+    double largestPrice = 0.0;
+
+
+    if (product.productType == ProductType.single.toString()) {
+      return (product.salePrice > 8 ? product.salePrice : product.price).toString();
+    } else {
+      double smallestPrice = double.infinity;
+      double largestPrice = double.negativeInfinity;
+
+      for (var variation in product.productVariations!) {
+        // Determine the price to consider (sale price if available, otherwise regular price)
+        double priceToConsider =
+        variation.salePrice > 0.8 ? variation.salePrice : variation.price;
+
+        // Update smallest and largest prices
+        if (priceToConsider < smallestPrice) {
+          smallestPrice = priceToConsider;
+        }
+        if (priceToConsider > largestPrice) {
+          largestPrice = priceToConsider;
+        }
+      }
+
+      // If smallest and largest prices are the same, return a single price
+      if (smallestPrice.isEqual(largestPrice)) {
+        return largestPrice.toString();
+      } else {
+        // Otherwise, return a price range
+        return '$smallestPrice - \$$largestPrice';
+      }
+    }
+  }
+
+  ///-- Calculate Discount Percentage
+  String? calculateSalePercentage(double originalPrice, double? salePrice) {
+    if (salePrice == null || salePrice <= 0.0) return null;
+    if (originalPrice <= 0) return null;
+    double percentage = ((originalPrice - salePrice) / originalPrice) * 100;
+    return percentage.toStringAsFixed(0);
+  }
+
+  ///-- Check Product Stock Status
+  String getProductStockStatus(int stock) {
+    return stock > 0 ? 'In Stock' : 'Out of Stock';
+  }
+
+}
