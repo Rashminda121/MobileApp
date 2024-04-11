5161857e62ebf0403ef0d31abf1cae36ac366caa
Onaliy Vinukiy Jayawardana <ovjayawardana@students.nsbm.ac.lk>
2024-04-11 22:50:56 +0530

    Create category_shimmer.dart

---
+++ lib/common/widgets/shimmers/category_shimmer.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/sizes.dart';
+
+class TCategoryShimmer extends StatelessWidget {
+  const TCategoryShimmer({
+    Key? key,
+    this.itemCount = 6,
+  }) : super(key: key);
+
+  final int itemCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      child: ListView.separated(
+        shrinkWrap: true,
+        itemCount: itemCount,
+        scrollDirection: Axis.horizontal,
+        separatorBuilder: (context, index) => const SizedBox(width: TSizes.spaceBtwItems),
+        itemBuilder: (context, index) {
+          return const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Image
+              TShimmerEffect(width: 55, height: 55, radius: 55),
+              SizedBox(height: TSizes.spaceBtwItems / 2),
+              // Text
+              TShimmerEffect(width: 55, height: 8),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
